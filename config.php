f05db5cea18e4900924105604eca654ccaef0682
Анна <117831324+Ann-Philippova@users.noreply.github.com>
2023-11-14 19:31:36 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$username: 'ch08756_content';
+$db_name: 'ch08756_content';
+$password: '777Scarpena777';
+
+$configs = ch08756_content("mysql:host=$host; db_name=$db_name", $username, $password);
+$sql="CREATE DATABASE ch08756_content"
+?>
